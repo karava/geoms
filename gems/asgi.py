ac1bee42906d0d5e0783635a3d993f90ceca9340
Kishan Arava <kishan.arava@gmail.com>
2021-11-09 18:29:48 +1100

    [UPDATE] Rename project to gems
    This is easier to pronounce and more catchy. It stands for Geosynetics electronic management system.

--- gems/asgi.py
+++ gems/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for geoms project.
+ASGI config for gems project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gems.settings')
 
 application = get_asgi_application()
--- gems/wsgi.py
+++ gems/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for geoms project.
+WSGI config for gems project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gems.settings')
 
 application = get_wsgi_application()
