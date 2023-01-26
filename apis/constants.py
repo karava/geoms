b0353e0cbe8bdd3f9d4486fbac67d16b9ad5dd3b
Kishan Arava <kishan.arava@gmail.com>
2023-01-26 21:02:57 +0800

    Check if contact exists and create invoice - 1st iteration

---
+++ apis/constants.py
@@ -0,0 +1,37 @@
+AU_BANK_DETAILS = """
+
+BANKING DETAILS:
+
+COMPANY: Geosynthetics Research Pty Ltd
+BANK: Commonwealth Bank
+BSB NO: 063 010
+ACCOUNT NO: 1453 3245
+"""
+
+US_BANK_DETAILS = """
+
+BANKING DETAILS:
+
+Please pay USD into the CBA account of:
+Geosynthetics Research PTY LTD
+Acc. No:      1453 9129  
+BSB  No:     063 010
+"""
+
+QUOTE_TERMS = """
+Quantity: Net quantities according to Packing List. Actual number of units will depend on the quantity that can be loaded to fill the container. 
+
+All other conditions as per our Standard Conditions attached.
+
+Please take note of the following condition which is also found in section 3 of the conditions attached.
+
+3.	Orders and Quotations - If not specified on a quote, a quote will be valid for 30 days from the date it is delivered to the Buyer or for such longer period as agreed by Geosynthetics Australia. Geosynthetics Australia may refuse an Order, or part of an Order placed by a Buyer at its discretion. An Order is deemed to have been accepted by Geosynthetics Australia when Geosynthetics Australia gives written notice to the Buyer, if Geosynthetics Australia delivers the Goods to the Buyer or the Buyer collects the Goods, whichever is the earlier.
+
+If Geosynthetics Australia accepts an Order it will deliver and the Buyer will purchase the Goods specified in the Order subject to these Terms, a Buyer cannot cancel an Order after Geosynthetics Australia accepts an Order, unless Geosynthetics Australia consents to the cancellation. If Geosynthetics Australia consents to the cancellation, the Buyer must pay Geosynthetics Australia on demand, all of the Geosynthetics Australia's direct and indirect costs of cancellation
+
+STORING OF GOODS ON SITE:
+
+Geotextiles must be protected against excessive ultraviolet radiation and contamination from dirt, dust, moisture, and any other deleterious materials or environmental conditions prior to correct onsite installation. 
+Geotextile rolls shall be adequately supported, stored elevated above the ground and
+remain wrapped in their opaque protective covering until the time of installation. Geotextiles that are intended to be covered by soil, rock, earth or other materials shall not be left exposed for more than 72 hours following the removal of the protective wrap. Geotextiles
+shall be protected from temperatures greater than 60°C."""
--- apis/views.py
+++ apis/views.py
@@ -3,23 +3,34 @@ from django.http import JsonResponse
 import requests
 from requests.auth import HTTPBasicAuth
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from apis import constants
+from datetime import datetime,timedelta
+import pytz
+import json
 
 
+@csrf_exempt
 def index(request):
-    XERO_ACCESS_TOKEN = fetchXeroAccessToken()
-    headers = {
-        'Authorization' : 'Bearer ' + XERO_ACCESS_TOKEN,
-        'Accept': 'application/json'
-        }
-    response = requests.get("https://api.xero.com/api.xro/2.0/Invoices", headers=headers)
-    if response.status_code == 200:
-        print("All good")
-    elif response.status_code == 401:
-        if "TokenExpired" in response.text:
-            print("401 error: Token expired")
-        
-    json_response = response.json()
-    return JsonResponse(json_response)
+    if request.method == "POST":
+        # quoteData = request.POST.copy()
+        quoteData = json.loads(request.body)
+        customer = quoteData.get("customer")
+        print(customer)
+    else:
+        print("invalid request")
+        return HttpResponse("Only POST request permitted")
+
+    # Check if customer already exists
+    contactID = checkIfCustomerExists(customer)
+    if contactID == False:
+        contactID = createNewContact(customerName=customer)
+    quoteData["contactID"] = contactID
+
+    # Create new quote
+    response = createNewQuote(quoteData=quoteData)
+
+    return JsonResponse(response, safe=False)
 
 def fetchXeroAccessToken():
     username = settings.XERO_CLIENT_ID
@@ -31,3 +42,89 @@ def fetchXeroAccessToken():
     response_data = response.json()
     token = response_data["access_token"]
     return token
+
+
+def checkIfCustomerExists(customerName):
+    response = prepareXeroAPICall(apiUrl=f'https://api.xero.com/api.xro/2.0/Contacts?where=Name="{customerName}"', method="GET")
+    if response["Contacts"] == []:
+        return False
+    else:
+        return response["Contacts"][0]["ContactID"]
+
+
+def createNewContact(customerName):
+    data = {"Name": customerName}
+
+    response = prepareXeroAPICall(apiUrl="https://api.xero.com/api.xro/2.0/Contacts", method="POST", data=data)
+    return response["Contacts"][0]["ContactID"]
+
+
+def prepareXeroAPICall(apiUrl, method, data=None):
+    XERO_ACCESS_TOKEN = fetchXeroAccessToken()
+    headers = {
+        'Authorization' : 'Bearer ' + XERO_ACCESS_TOKEN,
+        'Accept': 'application/json'
+    }
+
+    if method == "GET":
+        response = requests.get(apiUrl, headers=headers, json = data)
+    elif method == "POST":
+        print(data)
+        response = requests.post(apiUrl, headers=headers, json = data)
+
+    if response.status_code == 200:
+        print("All good")
+    elif response.status_code == 401:
+        if "TokenExpired" in response.text:
+            print("401 error: Token expired")
+
+    return response.json()
+
+
+def createNewQuote(quoteData):
+    summary = f"""Commodity: {quoteData["commodity"]}
+Pricing: {quoteData["pricing"]}
+Shipping details: {quoteData["shippingDetails"]}
+Packing Dimensions: {quoteData["packingDimensions"]}
+Unloading Details: {quoteData["unloadingDetails"]}
+Leadtime: {quoteData["leadTime"]}"""
+    
+    if quoteData.get('deliveryTerms', None) == "DDP":
+        bankDetails = constants.AU_BANK_DETAILS
+        taxType = "OUTPUT"
+        summary = summary + "\n" + f'Delivery address: {quoteData["deliveryAddress"]}' + "\n" + f'Unloading style: {quoteData["unloadingStyle"]}'
+    else:
+        bankDetails = constants.US_BANK_DETAILS
+        taxType = "EXEMPTOUTPUT"
+
+    currentDateTime = datetime.now(pytz.timezone('Australia/Victoria'))
+    today = str(currentDateTime.year)+"-"+str(currentDateTime.month)+"-"+str(currentDateTime.day)
+    expiryDateTime = currentDateTime + timedelta(days=14)
+    expiryDate = str(expiryDateTime.year)+"-"+str(expiryDateTime.month)+"-"+(str(expiryDateTime.day))
+
+    
+
+    data = {
+        "Terms": "Payment Terms: " + quoteData["paymentTerms"] + bankDetails + constants.QUOTE_TERMS,
+        "Contact": {
+            "ContactID": quoteData["contactID"],
+        },
+        "LineItems": [
+            {
+                "Description": quoteData["description"],
+                "UnitAmount": quoteData["unitPrice"],
+                "Quantity": quoteData["qty"],
+                "AccountCode": "200",
+                "TaxType" : taxType
+            }
+        ],
+        "Date": today,
+        "ExpiryDate": expiryDate,
+        "Title": quoteData["title"],
+        "Summary": summary,
+        "Tracking": [],
+        "LineAmountTypes": "EXCLUSIVE"
+    }
+
+    response = prepareXeroAPICall(apiUrl="https://api.xero.com/api.xro/2.0/Quotes", method="POST", data=data)
+    return response
