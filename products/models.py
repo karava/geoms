dcf56a16b5830c8a0e6dd8874ef3da4f0fa7c63e
Kishan Arava <kishan.arava@gmail.com>
2023-06-06 11:37:06 +0800

    Update GCL and baseproduct models

--- products/models.py
+++ products/models.py
@@ -63,7 +63,8 @@ class GCL(models.Model):
     suggested_applications = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return ("Density: " + str(self.density) + ", Roll width: " + str(self.roll_width))
+        base_product = self.baseproduct
+        return ("Code: " + str(base_product.code) + ", Density: " + str(self.density) + ", Roll width: " + str(self.roll_width))
 
 class Geotextile(models.Model):
     density = models.IntegerField()
@@ -100,7 +101,7 @@ class DrainageProduct(models.Model):
         return("Type: " + self.type + ", Height: " + str(self.height) + "x" + str(self.roll_width))
 
 class BaseProduct(models.Model):
-    code = models.CharField(max_length=10, blank=True)
+    code = models.CharField(max_length=200, blank=True)
     material = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
     suppliers = models.CharField(max_length=200, blank=True)
@@ -125,6 +126,7 @@ class Price(models.Model):
     qty = models.IntegerField
     unit_of_measure = models.CharField(choices=UNITS_OF_MEASURE, max_length=200, default='rolls')
     incoterm = models.CharField(choices=INCOTERMS, max_length=200, default='cif')
+    location = models.CharField(max_length=200, blank=True, null=True)
     currency = models.CharField(choices=CURRENCIES, max_length=200, default='usd')
     expiry = models.DateField(blank=True, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
