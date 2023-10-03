bd27034e82efa4e680350ac7f5845e09bfc0b00e
Kishan Arava <kishan.arava@gmail.com>
2023-10-03 20:56:34 +0800

    Add product sub categories view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 import os, json
 from django.conf import settings
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import BaseProduct
+from django.views.generic import DetailView
+from .models import BaseProduct, Geocell, Geogrid, Geotextile, GCL, DrainageProduct, ImageFile
+from django.db.models import Prefetch
 
 # Create your views here.
 
@@ -16,9 +17,42 @@ def index(request):
     context = {'categories': items, 'page_title': 'Product Categories'}
     return render(request, 'index.html', context)
 
+# def category(request, slug):
+#     context = {}
+#     return render(request, 'category.html', context)
+
 def category(request, slug):
-    context = {}
+    # Map slugs to their respective models and OneToOneField names in BaseProduct
+    slug_to_model = {
+        'geocells': (Geocell, 'product_detail_geocell'),
+        'gcls': (GCL, 'product_detail_gcl'),
+        'geotextiles': (Geotextile, 'product_detail_geotextile'),
+        'geogrids': (Geogrid, 'product_detail_geogrid'),
+        'drainage_systems': (DrainageProduct, 'product_detail_drainage'),
+    }
+
+    # Check if the provided slug is valid
+    if slug not in slug_to_model:
+        # Handle invalid slugs (you can render an error page or raise a 404)
+        return render(request, 'error_page.html', {'message': 'Invalid category.'})
+
+    # Fetch the related model and its field name in BaseProduct
+    related_model, related_field_name = slug_to_model[slug]
+
+    # Get all base products that have the related model set
+    products = BaseProduct.objects.filter(**{f"{related_field_name}__isnull": False})
+
+    # Prefetch related default images
+    default_images = ImageFile.objects.filter(is_default=True)
+    products = products.prefetch_related(Prefetch('images', queryset=default_images, to_attr='default_image'))
+
+    context = {
+        'products': products,
+        'category': related_model._meta.verbose_name_plural.title()
+    }
+    
     return render(request, 'category.html', context)
+
 
 class ProductDetailView(DetailView):
     model = BaseProduct
