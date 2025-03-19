6e276566a9ef81b5298c628440db13562226beeb
Kishan Arava <kishan.arava@gmail.com>
2025-03-20 07:44:44 +0800

    Add thumbnails and search to media admin

--- knowledge_base/admin.py
+++ knowledge_base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import TechnicalGuide, CaseStudy, Media, MediaRelation
 from django.contrib.contenttypes.admin import GenericTabularInline
 from tinymce.widgets import TinyMCE
+from django.utils.html import format_html
 
 class ImageInline(GenericTabularInline):  # You can also use admin.StackedInline for a different layout
     model = MediaRelation
@@ -27,6 +28,19 @@ class CaseStudyAdmin(admin.ModelAdmin):
             kwargs['widget'] = TinyMCE(attrs={'cols': 80, 'rows': 30})
         return super().formfield_for_dbfield(db_field, **kwargs)
 
+class MediaAdmin(admin.ModelAdmin):
+    search_fields = ("file", "description", "comment")
+    
+    def file_name(self, obj):
+        # Just call the model’s __str__ method
+        return str(obj)
+
+    def thumbnail(self, obj):
+        return format_html('<img src="{}" style="max-width:60px; max-height:60px"/>'.format(obj.file.url))
+    
+    thumbnail.short_description = 'Thumbnail'
+    list_display = ['file_name', 'thumbnail',]
+
 admin.site.register(TechnicalGuide, TechnicalGuideAdmin)
 admin.site.register(CaseStudy, CaseStudyAdmin)
-admin.site.register(Media)
+admin.site.register(Media, MediaAdmin)
